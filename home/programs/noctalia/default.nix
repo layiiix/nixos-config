f439c7a332aa19581b7d9dd917afd25362deacfa
layiiesp <galeonvo@protonmail.com>
2026-06-13 00:03:56 +0200

    noctallia

--- home/programs/noctalia/default.nix
+++ home/programs/noctalia/default.nix
@@ -6,6 +6,14 @@
       bar.main = {
         position = "top";
       };
+      shell = {
+        ui_scale = 1.25; # Aumentamos la escala para que no se vea tan pequeño
+      };
+      wallpaper = {
+        default = {
+          path = "/home/layiiesp/Pictures/Wallpapers/tokyo_night_owl.png";
+        };
+      };
       theme = {
         mode = "dark";
         source = "builtin";
